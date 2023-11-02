36690c61e4edaa390778e6bc568d57b9489c2d17
acer <acer@192.168.1.48>
2023-11-02 15:19:30 +0530

    first commit

---
+++ project1/src/com/tns/Overloding/OverlodingDemo.java
@@ -0,0 +1,13 @@
+package com.tns.Overloding;
+public class OverlodingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Point p1=new Point(9.8f);
+	System.out.println(p1);
+	Point p2=new Point(9.8f,9.8f);
+	System.out.println(p2);
+
+	}
+
+}
